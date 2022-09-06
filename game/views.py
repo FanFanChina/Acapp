9152cc2a1979f0fc8f03f6cda193f18c256e0654
fanfan <zhangfanfan@email.cn>
2022-09-06 20:33:33 +0800

    add my first page

--- game/views.py
+++ game/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("我的第一个网页！！！") 
+    line  = '<h1 style = "text-align: center">Hello World<h1>'
+    line1 = '<h1 style = "text-align: center">术士之战</h1>'
+    line2 = '<img src = "https://cdn.jsdelivr.net/gh/FanFanChina/StaticFiles/images/8.jpg">'
+    return HttpResponse(line + line1 + line2) 
+def play(request):
+    s = '<h1 style = "text-align: center">游戏界面</h1>'
+    return HttpResponse(s)
