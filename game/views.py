3f202ebd6cdf2ca672868518eb4b41639dbbc41e
fanfan <zhangfanfan@email.cn>
2022-09-07 20:48:10 +0800

    add menu background

--- game/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    line1 = '<h1>一号老婆<h1>'
-    line2 = '<img src = "https://cdn.jsdelivr.net/gh/FanFanChina/StaticFiles/images/8A8953536552D9121F1CFEEF2C8E601B.jpg">'
-    return HttpResponse(line1 + line2)
-def play(request):
-    s = '<h1 style = "text-align: center">Play Interface</h1>'
-    return HttpResponse(s)
